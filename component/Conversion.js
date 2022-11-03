49b33e9d8ac3ad7f98032dbed7f4b5b8fd098dca
Amirrak <mouzaoui.moncef@gmail.com>
2022-11-03 10:36:54 +0100

    Enable text fiel 'amount of money' when select the currency

--- component/Conversion.js
+++ component/Conversion.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import styles from "./styles/Conversion.module.css"
 import { TextField, Autocomplete } from '@mui/material';
 import CompareArrowsIcon from '@mui/icons-material/CompareArrows';
 
 export const Conversion = (props) => {
+    const [valueCurrencyA, setValueCurrencyA] = useState(0);
+    const [valueCurrencyB, setValueCurrencyB] = useState(0);
+    const [currencySelectedA, setCurrencySelectedA ] = useState("");
+    const [currencySelectedB, setCurrencySelectedB ] = useState("");
+    const [disabledValueCurrencyA, setDisabledValueCurrencyA ] = useState(true);
+    const [disabledValueCurrencyB, setDisabledValueCurrencyB ] = useState(true);
+
     var listCurrent = [];
     for(var key in props.currency){
         listCurrent.push(props.currency[key])
@@ -13,6 +20,14 @@ export const Conversion = (props) => {
            <div className={styles.tableau}>
                 <Autocomplete
                     className={styles.currencyA}
+                    onChange={(e, value)=>{
+                        setCurrencySelectedA(value)
+                        if(value!== "" && value !== undefined && value!==null) {
+                            setDisabledValueCurrencyA(false)
+                        }else {
+                            setDisabledValueCurrencyA(true)
+                        }
+                    }}
                     disablePortal
                     id="combo-box-demo"
                     options={listCurrent}
@@ -25,6 +40,14 @@ export const Conversion = (props) => {
 
                 <Autocomplete
                     className={styles.currencyB}
+                    onChange={(e, value)=>{
+                        setCurrencySelectedB(value)
+                        if(value!== "" && value !== undefined && value!==null) {
+                            setDisabledValueCurrencyB(false)
+                        }else {
+                            setDisabledValueCurrencyB(true)
+                        }
+                    }}
                     disablePortal
                     id="combo-box-demo"
                     options={listCurrent}
@@ -32,11 +55,11 @@ export const Conversion = (props) => {
                 />
 
                 <div className={styles.amountA}>
-                    <TextField label="Number" type="number" variant="outlined" sx={{width:'100%'}} />
+                    <TextField disabled={disabledValueCurrencyA} label="Number" type="number" variant="outlined" sx={{width:'100%'}} value={valueCurrencyA} onChange={(e)=>setValueCurrencyA(e.target.value)} />
                 </div>
 
                 <div className={styles.amountB}>
-                    <TextField label="Number" type="number" variant="outlined" sx={{width:'100%'}} />
+                    <TextField disabled={disabledValueCurrencyB} label="Number" type="number" variant="outlined" sx={{width:'100%'}} value={valueCurrencyA} />
                 </div>
 
             </div>
