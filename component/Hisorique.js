537ebbde5146edc69bcadfedaf409d70b22a8b23
Amirrak <mouzaoui.moncef@gmail.com>
2022-11-16 15:38:35 +0100

    cast data recu et les passer et les affichers avec Redcharts

--- component/Hisorique.js
+++ component/Hisorique.js
@@ -6,7 +6,7 @@ import { TextField, Autocomplete, Button } from '@mui/material';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
 
 
 
@@ -16,6 +16,8 @@ export default function Historique(props) {
     const [currencyHistoric, setCurrencyHistoric] = useState("")
     const [currencyRef, setCurrencyRef] = useState("")
     const [rateCurrency, setRateCurrency] = useState({})
+
+    const [data, setData] = useState();
 
     const [from, setFrom] = useState(
         dayjs().subtract(1,'day'),
@@ -45,16 +47,23 @@ export default function Historique(props) {
         axios.get(req)
         .then(function (response) {
             setRateCurrency(response.data.rates)
-            console.log(rateCurrency)
         })
         .catch(function (error) {
             console.log(error)
         });
     }
-    
-    useEffect(()=>{ console.log(rateCurrency) },[rateCurrency])
-    const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page B', uv: 200, pv: 2400, amt: 2400}, {name: 'Page C', uv: 400, pv: 2400, amt: 2400}];
-
+    useEffect(()=>{
+        if(Object.keys(rateCurrency).length !== 0) {
+            console.log(Object.keys(rateCurrency).length)
+            console.log('NAME ', Object.keys(rateCurrency)[1])
+            console.log('UV ', rateCurrency[ Object.keys(rateCurrency)[1] ][ Object.keys(rateCurrency[Object.keys(rateCurrency)[0]]) ])
+            var dataTMP = [];
+            for(var i = 0; i<(Object.keys(rateCurrency).length); i++) {
+                dataTMP.push({name: Object.keys(rateCurrency)[i], uv: rateCurrency[ Object.keys(rateCurrency)[i] ][ Object.keys(rateCurrency[Object.keys(rateCurrency)[0]]) ] })
+            }
+            setData(dataTMP)
+        }
+    },[rateCurrency])
     return (
         <div className={styles.container}>
             <div className={styles.containerForm}>
@@ -98,10 +107,11 @@ export default function Historique(props) {
                     <Button variant="contained" className={styles.bouton} onClick={showHistoric}>Contained</Button>
             </div>
             <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                <Line type="monotone" dataKey="uv" stroke="blue" dot={false}/>
                 <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                <XAxis dataKey="name" />
+                <XAxis dataKey="name" tick={false} label="Time" />
                 <YAxis />
+                <Tooltip />
             </LineChart>
         </div>
     );
