5570d4ded222501908665c142d742dfd7cac9221
Amirrak <79013044+Amirrak@users.noreply.github.com>
2023-06-23 17:07:29 +0100

    Delete docs directory

--- docs/AXIx_j21I9RSVY1UCz_On/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/928-6f1bd96687efbe0a.js","static/css/33d67baf49c91c2e.css","static/chunks/pages/index-6974ba63ef24a46b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
